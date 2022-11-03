92628d22e1fd10cb6c861f46c386ab73f6cbd0e9
Jason Hall <jason@chainguard.dev>
2022-11-03 10:56:20 -0400

    add check that Makefile is up to date
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- lint.sh
+++ lint.sh
@@ -4,6 +4,17 @@ set -uo pipefail
 
 for f in *.yaml; do
   echo "---" $f
+
+  # Check that every name-version-epoch is defined in Makefile
+  name=$(yq '.package.name' $f)
+  version=$(yq '.package.version' $f)
+  epoch=$(yq '.package.epoch' $f)
+  want="build-package,${name},${version}-r${epoch}"
+  if ! grep -q $want Makefile; then
+    echo "missing $want in Makefile"
+    exit 1
+  fi
+
   # Remove repositories and keys defined in the YAML; these should only be
   # appended at build-time to the local packages.
   yq -i 'del(.environment.contents.repositories)' $f

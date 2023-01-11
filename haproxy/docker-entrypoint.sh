34ce5241369edcdabce479ee7fe6944ee24456ac
Tuan Anh Tran <me@tuananh.org>
2023-01-11 22:26:03 +0700

    add haproxy-oci-entrypoint subpkg
    Signed-off-by: Tuan Anh Tran <me@tuananh.org>

---
+++ haproxy/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- haproxy "$@"
+fi
+
+if [ "$1" = 'haproxy' ]; then
+	shift # "haproxy"
+	# if the user wants "haproxy", let's add a couple useful flags
+	#   -W  -- "master-worker mode" (similar to the old "haproxy-systemd-wrapper"; allows for reload via "SIGUSR2")
+	#   -db -- disables background mode
+	set -- haproxy -W -db "$@"
+fi
+
+exec "$@"

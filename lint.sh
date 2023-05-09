9fe1fbe228d4cad9b43771dabd63540161f5ca70
Dan Luhring <dluhring@chainguard.dev>
2023-05-09 16:24:08 -0400

    Add lint check to ban advisory data
    Signed-off-by: Dan Luhring <dluhring@chainguard.dev>

--- lint.sh
+++ lint.sh
@@ -20,4 +20,14 @@ for f in *.yaml; do
     yq -i 'del(.environment.contents.repositories)' $f
     yq -i 'del(.environment.contents.keyring)' $f
   fi
+
+  # With the introduction of https://github.com/wolfi-dev/advisories,
+  # package config files should no longer contain any advisory data.
+  if [[ "$(yq 'keys | contains(["advisories"])' "$f")" == "true" ]]; then
+    echo "
+$f has an 'advisories' section, but advisory data should now be stored in https://github.com/wolfi-dev/advisories.
+
+To learn about how to create advisory data in the advisories repo, run 'wolfictl advisory create -h', and check out the '--advisories-repo-dir' flag."
+    exit 1
+  fi
 done

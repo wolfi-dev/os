35cc31709a493dc42d3ce3e92c279c7c4704dd83
RJ Sampson <rj.sampson@chainguard.dev>
2024-04-06 10:15:57 -0600

    chore(conda-init): Create base environment if it doesn't exist
    Creates a base environment after initializing conda if one can't be
    found. This is user agnostic however, the conda-base package should
    still be maintained to cut down on time to entry - this is a bandaid
    for nonroot users
    
    Signed-off-by: RJ Sampson <rj.sampson@chainguard.dev>

--- conda/init.sh
+++ conda/init.sh
@@ -2,5 +2,10 @@
 if [[ ! -f ~/.conda-initialized ]]; then
   conda init
   source ~/.bashrc
+  # Create base env if it doesn't exist
+  if [[ ! -d ~/conda ]]; then
+    conda config --set root_prefix ~/conda
+    conda create -n base
+  fi
   touch ~/.conda-initialized
 fi

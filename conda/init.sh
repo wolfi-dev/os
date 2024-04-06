3bc3da13218bc17cd427298ef51c180b42f709ee
RJ Sampson <rj.sampson@chainguard.dev>
2024-04-06 10:24:54 -0600

    chore(conda-init): Redirect fd1 to /dev/null
    Initialization should appear seamless unless there's an error
    
    Signed-off-by: RJ Sampson <rj.sampson@chainguard.dev>

--- conda/init.sh
+++ conda/init.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 if [[ ! -f ~/.conda-initialized ]]; then
-  conda init
-  source ~/.bashrc
+  conda init 1>/dev/null
+  source ~/.bashrc 1>/dev/null
   # Create base env if it doesn't exist
   if [[ ! -d ~/conda ]]; then
-    conda config --set root_prefix ~/conda
-    conda create -n base
+    conda config --set root_prefix ~/conda 1>/dev/null
+    conda create -n base 1>/dev/null
   fi
-  touch ~/.conda-initialized
+  touch ~/.conda-initialized 1>/dev/null
 fi

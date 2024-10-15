392a027421cfd79c84d146c6a2d918553ba9d4aa
uti <uti.edun@chainguard.dev>
2024-10-15 13:25:09 +0000

    Adding service path

--- geckodriver/test-geckodriver.py
+++ geckodriver/test-geckodriver.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service
 options = webdriver.FirefoxOptions()
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
@@ -7,8 +9,8 @@ options.add_argument(
     "--headless"
 )  # Run in headless mode to avoid opening a browser window
 
-
-driver = webdriver.Firefox(options=options)
+service = Service(executable_path='/usr/bin/geckodriver')
+driver = webdriver.Firefox(service=service, options=options)
 
 driver.get("https://selenium.dev/")
 assert "Selenium" in driver.title

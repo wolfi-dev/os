014e356b9747c0ef7709f27d232ee6a653e9b455
Matthew Ramirez <matthew.ramirez@chainguard.dev>
2025-02-25 15:47:29 -0800

    Add entrypoint and config files to match upstream liquibase
    Signed-off-by: Matthew Ramirez <matthew.ramirez@chainguard.dev>

---
+++ liquibase/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+if [[ "$INSTALL_MYSQL" ]]; then
+  lpm add mysql --global
+fi
+
+if [[ "$1" != "history" ]] && [[ "$1" != "init" ]] && type "$1" > /dev/null 2>&1; then
+  ## First argument is an actual OS command (except if the command is history or init as it is a liquibase command). Run it
+  exec "$@"
+else
+  if [[ "$*" == *--defaultsFile* ]] || [[ "$*" == *--defaults-file* ]] || [[ "$*" == *--version* ]]; then
+    ## Just run as-is
+    exec /liquibase/liquibase "$@"
+  else
+    ## Include standard defaultsFile
+    exec /liquibase/liquibase "--defaultsFile=/liquibase/liquibase.docker.properties" "$@"
+  fi
+fi

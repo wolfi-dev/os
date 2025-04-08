650b688285c385de486bebf67db5723c9c16c2be
Jason Hall <jason@chainguard.dev>
2025-04-08 15:00:56 -0400

    revert lint do not delete it
    Signed-off-by: Jason Hall <jason@chainguard.dev>

---
+++ lint.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# If arguments are passed to the command, only lint the files listed.
+if [ "$#" == 0 ]; then
+  list="*.yaml"
+else
+  list=$*
+fi
+
+for fn in $list; do
+  case $fn in *.yaml) ;; *) echo "--- $fn not a yaml file, skipping"; continue ;; esac
+
+  p=$(yq -r '.package.name' ${fn})
+  echo "--- package" $p
+
+  # Don't specify repositories or keyring for os packages
+  if grep -q packages.wolfi.dev/os ${fn}; then
+    yq -i 'del(.environment.contents.repositories)' ${fn}
+    yq -i 'del(.environment.contents.keyring)' ${fn}
+  fi
+
+  # Don't specify wolfi-base or any of its packages, or the main package, for test pipelines.
+  for pkg in wolfi-base busybox apk-tools wolfi-keys ${p}; do
+    yq -i 'del(.test.environment.contents.packages[] | select(. == "'${pkg}'"))' ${fn}
+  done
+
+  # If .test.environment.contents.packages is empty, remove it all.
+  if [ "$(yq -r '.test.environment.contents.packages | length' ${fn})" == "0" ]; then
+    yq -i 'del(.test.environment.contents)' ${fn}
+  fi
+
+  yam ${fn}
+done
+
+# New section to check for .sts.yaml files under ./.github/chainguard/
+echo "Checking for .sts.yaml files in ./.github/chainguard/..."
+for file in $(find .github/chainguard -type f); do
+  if [[ ! $file =~ \.sts\.yaml$ ]]; then
+    echo "ERROR: File $file does not have the required '.sts.yaml' suffix"
+    exit 1
+  fi
+done

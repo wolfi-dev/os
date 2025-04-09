8aa6bbdf1365279610bab87f4e86fa90f31de7df
Ville Aikas <vaikas@chainguard.dev>
2025-04-08 18:17:54 -0700

    bug: Do not keep approving already approved prs.
    Signed-off-by: Ville Aikas <vaikas@chainguard.dev>

--- scripts/auto-approve-pr.sh
+++ scripts/auto-approve-pr.sh
@@ -7,7 +7,8 @@ set -o pipefail
 repo=$1
 
 get_prs() {
-    gh pr list --app "octo-sts" --label "auto-approver-bot/approve" --limit=50 --json number --jq '.[].number'
+    # Get PRs that are not approved by the bot (github-actions)
+    gh pr list --app "octo-sts" --label "auto-approver-bot/approve" --json number,reviews --limit 100 --jq '.[] | select(.reviews | map(.author.login == "github-actions") | all(. == false)) | .number'
 }
 
 readarray -t PRS < <(get_prs)

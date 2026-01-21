d7189f0dc18f32e6db4dc39a3d0f43ed3cdd1435
Debasish Biswas <debasishbsws.dev@gmail.com>
2026-01-21 21:10:10 +0530

    Merge branch 'main' into debasishbsws-71253f6712
    Export:  5960c8ac19398bcf5a4d466f325d1502e4c156ca

--- openjdk-8/basic/RequestTest.java
+++ openjdk-8/basic/RequestTest.java
@@ -10,6 +10,8 @@ public class RequestTest {
         while (true) {
             URL url = new URL("http://example.org");
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            // Cloudflare sometimes blocks Java<10 User-Agent
+            urlConnection.setRequestProperty("User-Agent", "Chainguard Test");
             if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
                 break;
             }
